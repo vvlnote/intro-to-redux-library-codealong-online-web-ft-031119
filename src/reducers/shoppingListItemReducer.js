191efe50f3251327060ccb1d6e2423f97903f7a8
Vivvie Lin <vvlnote@hotmail.com>
2019-08-06 16:48:45 -0700

    use redux library - createStore, and add redux-devtools-extension to the createStore()

--- src/reducers/shoppingListItemReducer.js
+++ src/reducers/shoppingListItemReducer.js
@@ -1,14 +1,12 @@
-export default function shoppingListItemReducer(
-	state = {
-		items: []
-	},
-	action
-) {
+export default function shoppingListItemReducer(state = {items: []}, action) {
 	switch (action.type) {
 		case 'INCREASE_COUNT':
-			return state.items.concat(state.items.length + 1);
+			console.log(`Current state.items length is ${state.items.length}`);
+			console.log(`updating state.itmes legnth to ${state.items.length+1}`);
+			return Object.assign({}, state, {items: state.items.concat(state.items.length + 1)});
 
 		default:
+			console.log(`Initial state.items length: ${state.items.length}` );
 			return state;
 	}
 }
